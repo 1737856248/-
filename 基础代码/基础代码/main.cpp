d0e75b8a077ef2cd60486d63256f8d4ef0870636
kiss灬旋律 卜 恋 <1737856248qq@.com>
2020-05-28 12:33:53 +0800

    实现基础代码管理

---
+++ 基础代码/基础代码/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#define q 3;
+using namespace std;
+int main(void) {
+
+	cout << "ÎÒÏ²»¶Äã" << endl;
+	system("pause");
+	return 0;
+
+}
